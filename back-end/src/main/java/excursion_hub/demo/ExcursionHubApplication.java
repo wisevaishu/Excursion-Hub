cf580e85c7c2b0589d000f7ccf42eb4647b19381
wisevaishu <vpm.win@gmail.com>
2025-07-06 13:17:53 -0500

    first commit in excursion-hub

---
+++ back-end/src/main/java/excursion_hub/demo/ExcursionHubApplication.java
@@ -0,0 +1,13 @@
+package excursion_hub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcursionHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcursionHubApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/excursion_hub/demo/ExcursionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package excursion_hub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcursionHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
