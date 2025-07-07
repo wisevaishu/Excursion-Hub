c6575a7e2cb19630b7d3503947f95ab40e519641
wisevaishu <vpm.win@gmail.com>
2025-07-07 11:24:46 -0500

    Added Application Properties, config file and set up dependencies

---
+++ back-end/src/main/java/excursion_hub/demo/configurations/WebConfig.java
@@ -0,0 +1,19 @@
+package excursion_hub.demo.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableTransactionManagement
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
